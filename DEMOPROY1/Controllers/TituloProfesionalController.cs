a04a6251e16735818b5cd903783d8e8b0b0da0ae
DestroyerC7 <160562685+DestroyerC7@users.noreply.github.com>
2024-08-27 20:25:00 -0400

    Controladores

---
+++ DEMOPROY1/Controllers/TituloProfesionalController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_08_2024.Controladores
+{
+    public class TituloProfesionalController
+    {/*
+        private List<TituloProfesional> titulosProfesionales = new List<TituloProfesional>();
+
+        // Método para agregar un título profesional
+        public void AgregarTituloProfesional(TituloProfesional titulo)
+        {
+            titulosProfesionales.Add(titulo);
+        }
+
+        // Método para obtener todos los títulos profesionales
+        public List<TituloProfesional> ObtenerTitulosProfesionales()
+        {
+            return titulosProfesionales;
+        }
+
+        // Método para actualizar un título profesional
+        public void ActualizarTituloProfesional(TituloProfesional titulo)
+        {
+            var tituloExistente = titulosProfesionales.FirstOrDefault(t => t.IdTitulo == titulo.IdTitulo);
+            if (tituloExistente != null)
+            {
+                tituloExistente.Nombre = titulo.Nombre;
+                tituloExistente.Descripcion = titulo.Descripcion;
+                tituloExistente.Institucion = titulo.Institucion;
+                tituloExistente.FechaObtencion = titulo.FechaObtencion;
+                tituloExistente.NivelAcademico = titulo.NivelAcademico;
+                tituloExistente.Especializacion = titulo.Especializacion;
+                tituloExistente.RegistroProfesional = titulo.RegistroProfesional;
+                tituloExistente.FechaRegistro = titulo.FechaRegistro;
+            }
+        }
+
+        // Método para eliminar un título profesional
+        public void EliminarTituloProfesional(int id)
+        {
+            var titulo = titulosProfesionales.FirstOrDefault(t => t.IdTitulo == id);
+            if (titulo != null)
+            {
+                titulosProfesionales.Remove(titulo);
+            }
+        }*/
+    }
+}
---
+++ DEMOPROY1/Controllers/TribunalController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _25_08_2024.Modelos;
+using Microsoft.Data.SqlClient;
+
+namespace _25_08_2024.Controladores
+{
+    public class TribunalController
+    {
+        private string connectionString = "server=DESTROYER; database=DEMOPROY; Integrated Security=True; TrustServerCertificate=True;"; // Reemplaza esto con tu cadena de conexión a la base de datos
+
+        // Método para agregar un tribunal
+        public void AgregarTribunal(Tribunal tribunal)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "INSERT INTO TRIBUNAL (PrimerNombreT, SegundoNombreT, PrimerApellidoT, SegundoApellidoT, Tipo, Institucion, Id_titulo) " +
+                               "VALUES (@PrimerNombreT, @SegundoNombreT, @PrimerApellidoT, @SegundoApellidoT, @Tipo, @Institucion, @Id_titulo)";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@PrimerNombreT", tribunal.PrimerNombreT);
+                    cmd.Parameters.AddWithValue("@SegundoNombreT", (object)tribunal.SegundoNombreT ?? DBNull.Value);
+                    cmd.Parameters.AddWithValue("@PrimerApellidoT", tribunal.PrimerApellidoT);
+                    cmd.Parameters.AddWithValue("@SegundoApellidoT", (object)tribunal.SegundoApellidoT ?? DBNull.Value);
+                    cmd.Parameters.AddWithValue("@Tipo", tribunal.Tipo);
+                    cmd.Parameters.AddWithValue("@Institucion", tribunal.Institucion);
+                    cmd.Parameters.AddWithValue("@Id_titulo", tribunal.Id_titulo);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Método para obtener todos los tribunales
+        public List<Tribunal> ObtenerTribunales()
+        {
+            List<Tribunal> tribunales = new List<Tribunal>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT * FROM TRIBUNAL";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Tribunal tribunal = new Tribunal
+                        {
+                            Id_Tribunal = (int)reader["Id_Tribunal"],
+                            PrimerNombreT = (string)reader["PrimerNombreT"],
+                            SegundoNombreT = reader["SegundoNombreT"] as string,
+                            PrimerApellidoT = (string)reader["PrimerApellidoT"],
+                            SegundoApellidoT = reader["SegundoApellidoT"] as string,
+                            Tipo = (string)reader["Tipo"],
+                            Institucion = (string)reader["Institucion"],
+                            Id_titulo = (int)reader["Id_titulo"]
+                        };
+                        tribunales.Add(tribunal);
+                    }
+                }
+            }
+
+            return tribunales;
+        }
+
+        // Método para actualizar un tribunal
+        public void ActualizarTribunal(Tribunal tribunal)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "UPDATE TRIBUNAL SET PrimerNombreT = @PrimerNombreT, SegundoNombreT = @SegundoNombreT, " +
+                               "PrimerApellidoT = @PrimerApellidoT, SegundoApellidoT = @SegundoApellidoT, Tipo = @Tipo, " +
+                               "Institucion = @Institucion, Id_titulo = @Id_titulo WHERE Id_Tribunal = @Id_Tribunal";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Id_Tribunal", tribunal.Id_Tribunal);
+                    cmd.Parameters.AddWithValue("@PrimerNombreT", tribunal.PrimerNombreT);
+                    cmd.Parameters.AddWithValue("@SegundoNombreT", (object)tribunal.SegundoNombreT ?? DBNull.Value);
+                    cmd.Parameters.AddWithValue("@PrimerApellidoT", tribunal.PrimerApellidoT);
+                    cmd.Parameters.AddWithValue("@SegundoApellidoT", (object)tribunal.SegundoApellidoT ?? DBNull.Value);
+                    cmd.Parameters.AddWithValue("@Tipo", tribunal.Tipo);
+                    cmd.Parameters.AddWithValue("@Institucion", tribunal.Institucion);
+                    cmd.Parameters.AddWithValue("@Id_titulo", tribunal.Id_titulo);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Método para eliminar un tribunal
+        public void EliminarTribunal(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "DELETE FROM TRIBUNAL WHERE Id_Tribunal = @Id_Tribunal";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Id_Tribunal", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Método para obtener un tribunal por su ID
+        public Tribunal ObtenerTribunalPorId(int id)
+        {
+            Tribunal tribunal = null;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT * FROM TRIBUNAL WHERE Id_Tribunal = @Id_Tribunal";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Id_Tribunal", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            tribunal = new Tribunal
+                            {
+                                Id_Tribunal = (int)reader["Id_Tribunal"],
+                                PrimerNombreT = (string)reader["PrimerNombreT"],
+                                SegundoNombreT = reader["SegundoNombreT"] as string,
+                                PrimerApellidoT = (string)reader["PrimerApellidoT"],
+                                SegundoApellidoT = reader["SegundoApellidoT"] as string,
+                                Tipo = (string)reader["Tipo"],
+                                Institucion = (string)reader["Institucion"],
+                                Id_titulo = (int)reader["Id_titulo"]
+                            };
+                        }
+                    }
+                }
+            }
+
+            return tribunal;
+        }
+    }
+
+}
