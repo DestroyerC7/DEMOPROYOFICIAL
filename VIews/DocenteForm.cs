3d2961e1c2b651e477826bd06682d6a830825e57
DestroyerC7 <carlosfernandolopez12@gmail.com>
2024-08-27 21:42:35 -0400

    views

---
+++ VIews/DocenteForm.cs
@@ -0,0 +1,112 @@
+﻿using _26_08_2024.Controladores;
+using DEMOPROY1.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DEMOPROY1.VIews
+{
+    public partial class DocenteForm : Form
+    {
+        public DocenteForm()
+        {
+            InitializeComponent();
+        }
+        private DocenteController controller = new DocenteController();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ActualizarListaDocentes();
+        }
+        private void ActualizarListaDocentes()
+        {
+            var docentes = controller.ObtenerDocentes();
+            dtgDocentes.DataSource = null;
+            dtgDocentes.DataSource = docentes;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnagregar_Click(object sender, EventArgs e)
+        {
+            var docente = new Docente
+            {
+                PrimerNombre = txtPrimerNombre.Text,
+                SegundoNombre = txtSegundoNombre.Text,
+                PrimerApellido = txtPrimerApellido.Text,
+                SegundoApellido = txtSegundoApellido.Text,
+                Email = txtEmail.Text,
+                //Id_Titulo = (int)comboBoxTitulo.SelectedValue
+            };
+
+            controller.AgregarDocente(docente);
+            ActualizarListaDocentes();
+            MessageBox.Show("Docente agregado exitosamente");
+        }
+
+        private void btnactualizar_Click(object sender, EventArgs e)
+        {
+            if (dtgDocentes.SelectedRows.Count > 0)
+            {
+                var selectedRow = dtgDocentes.SelectedRows[0];
+                var docente = (Docente)selectedRow.DataBoundItem;
+
+                docente.PrimerNombre = txtPrimerNombre.Text;
+                docente.SegundoNombre = txtSegundoNombre.Text;
+                docente.PrimerApellido = txtPrimerApellido.Text;
+                docente.SegundoApellido = txtSegundoApellido.Text;
+                docente.Email = txtEmail.Text;
+                //docente.Id_Titulo = (int)comboBoxTitulo.SelectedValue;
+
+                controller.ActualizarDocente(docente);
+                ActualizarListaDocentes();
+                MessageBox.Show("Docente actualizado exitosamente");
+            }
+            else
+            {
+                MessageBox.Show("Selecciona un docente para actualizar");
+            }
+        }
+
+        private void btneliminar_Click(object sender, EventArgs e)
+        {
+            if (dtgDocentes.SelectedRows.Count > 0)
+            {
+                var selectedRow = dtgDocentes.SelectedRows[0];
+                var docente = (Docente)selectedRow.DataBoundItem;
+
+                controller.EliminarDocente(docente.Id_Docente);
+                ActualizarListaDocentes();
+                MessageBox.Show("Docente eliminado exitosamente");
+            }
+            else
+            {
+                MessageBox.Show("Selecciona un docente para eliminar");
+            }
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            txtPrimerNombre.Text = string.Empty;
+            txtSegundoNombre.Text = string.Empty;
+            txtPrimerApellido.Text = string.Empty;
+            txtSegundoApellido.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+            //comboBoxTitulo.SelectedIndex = -1;
+        }
+    }
+}
---
+++ VIews/TribunalForm.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace DEMOPROY1.VIews
+{
+    partial class TribunalForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.btnactualizar = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.dtgTribunales = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtPrimerNombre = new System.Windows.Forms.TextBox();
+            this.txtSegundoNombre = new System.Windows.Forms.TextBox();
+            this.txtPrimerApellido = new System.Windows.Forms.TextBox();
+            this.txtSegundoApellido = new System.Windows.Forms.TextBox();
+            this.txtTipo = new System.Windows.Forms.TextBox();
+            this.txtInstitucion = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgTribunales)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.Location = new System.Drawing.Point(31, 501);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(98, 37);
+            this.btnagregar.TabIndex = 0;
+            this.btnagregar.Text = "AGREGAR";
+            this.btnagregar.UseVisualStyleBackColor = true;
+            this.btnagregar.Click += new System.EventHandler(this.btnagregar_Click);
+            // 
+            // btnactualizar
+            // 
+            this.btnactualizar.Location = new System.Drawing.Point(157, 501);
+            this.btnactualizar.Name = "btnactualizar";
+            this.btnactualizar.Size = new System.Drawing.Size(119, 37);
+            this.btnactualizar.TabIndex = 1;
+            this.btnactualizar.Text = "ACTUALIZAR";
+            this.btnactualizar.UseVisualStyleBackColor = true;
+            this.btnactualizar.Click += new System.EventHandler(this.btnactualizar_Click);
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.Location = new System.Drawing.Point(305, 501);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(92, 37);
+            this.btneliminar.TabIndex = 2;
+            this.btneliminar.Text = "ELIMINAR";
+            this.btneliminar.UseVisualStyleBackColor = true;
+            this.btneliminar.Click += new System.EventHandler(this.btneliminar_Click);
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.Location = new System.Drawing.Point(425, 501);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(83, 37);
+            this.btnlimpiar.TabIndex = 3;
+            this.btnlimpiar.Text = "LIMPIAR";
+            this.btnlimpiar.UseVisualStyleBackColor = true;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // dtgTribunales
+            // 
+            this.dtgTribunales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgTribunales.Location = new System.Drawing.Point(31, 297);
+            this.dtgTribunales.Name = "dtgTribunales";
+            this.dtgTribunales.RowHeadersWidth = 51;
+            this.dtgTribunales.RowTemplate.Height = 24;
+            this.dtgTribunales.Size = new System.Drawing.Size(733, 187);
+            this.dtgTribunales.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(274, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "REGISTRO DE TRIBUNAL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Primer Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Segundo Nombre:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(432, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Primer Apellido:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(432, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Segundo Apellido:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 178);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Tipo:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(432, 178);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 16);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Institución:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(28, 242);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 16);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Titulo:";
+            // 
+            // txtPrimerNombre
+            // 
+            this.txtPrimerNombre.Location = new System.Drawing.Point(176, 49);
+            this.txtPrimerNombre.Name = "txtPrimerNombre";
+            this.txtPrimerNombre.Size = new System.Drawing.Size(100, 22);
+            this.txtPrimerNombre.TabIndex = 13;
+            // 
+            // txtSegundoNombre
+            // 
+            this.txtSegundoNombre.Location = new System.Drawing.Point(176, 113);
+            this.txtSegundoNombre.Name = "txtSegundoNombre";
+            this.txtSegundoNombre.Size = new System.Drawing.Size(100, 22);
+            this.txtSegundoNombre.TabIndex = 14;
+            // 
+            // txtPrimerApellido
+            // 
+            this.txtPrimerApellido.Location = new System.Drawing.Point(583, 49);
+            this.txtPrimerApellido.Name = "txtPrimerApellido";
+            this.txtPrimerApellido.Size = new System.Drawing.Size(100, 22);
+            this.txtPrimerApellido.TabIndex = 15;
+            // 
+            // txtSegundoApellido
+            // 
+            this.txtSegundoApellido.Location = new System.Drawing.Point(583, 113);
+            this.txtSegundoApellido.Name = "txtSegundoApellido";
+            this.txtSegundoApellido.Size = new System.Drawing.Size(100, 22);
+            this.txtSegundoApellido.TabIndex = 16;
+            // 
+            // txtTipo
+            // 
+            this.txtTipo.Location = new System.Drawing.Point(176, 175);
+            this.txtTipo.Name = "txtTipo";
+            this.txtTipo.Size = new System.Drawing.Size(100, 22);
+            this.txtTipo.TabIndex = 17;
+            // 
+            // txtInstitucion
+            // 
+            this.txtInstitucion.Location = new System.Drawing.Point(583, 175);
+            this.txtInstitucion.Name = "txtInstitucion";
+            this.txtInstitucion.Size = new System.Drawing.Size(100, 22);
+            this.txtInstitucion.TabIndex = 18;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(176, 239);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 19;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // TribunalForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 550);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtInstitucion);
+            this.Controls.Add(this.txtTipo);
+            this.Controls.Add(this.txtSegundoApellido);
+            this.Controls.Add(this.txtPrimerApellido);
+            this.Controls.Add(this.txtSegundoNombre);
+            this.Controls.Add(this.txtPrimerNombre);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtgTribunales);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.btneliminar);
+            this.Controls.Add(this.btnactualizar);
+            this.Controls.Add(this.btnagregar);
+            this.Name = "TribunalForm";
+            this.Text = "TribunalForm";
+            this.Load += new System.EventHandler(this.TribunalForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgTribunales)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnagregar;
+        private System.Windows.Forms.Button btnactualizar;
+        private System.Windows.Forms.Button btneliminar;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.DataGridView dtgTribunales;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtPrimerNombre;
+        private System.Windows.Forms.TextBox txtSegundoNombre;
+        private System.Windows.Forms.TextBox txtPrimerApellido;
+        private System.Windows.Forms.TextBox txtSegundoApellido;
+        private System.Windows.Forms.TextBox txtTipo;
+        private System.Windows.Forms.TextBox txtInstitucion;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
