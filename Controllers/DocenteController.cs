9de4df47c27e67014e0b2529165c89971d64fae7
DestroyerC7 <carlosfernandolopez12@gmail.com>
2024-08-28 23:42:18 -0400

    Antes de morir

--- Controllers/DocenteController.cs
+++ Controllers/DocenteController.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using DEMOPROY1.Models;
 using System;
 using System.Collections.Generic;
@@ -158,3 +159,56 @@ namespace _26_08_2024.Controladores
         }
     }
 }
+=======
+﻿using _26_08_2024.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_08_2024.Controladores
+{
+    public class DocenteController
+    {
+        private List<Docente> docentes = new List<Docente>();
+
+        // Método para agregar un docente
+        public void AgregarDocente(Docente docente)
+        {
+            docentes.Add(docente);
+        }
+
+        // Método para obtener todos los docentes
+        public List<Docente> ObtenerDocentes()
+        {
+            return docentes;
+        }
+
+        // Método para actualizar un docente
+        public void ActualizarDocente(Docente docente)
+        {
+            var existingDocente = docentes.FirstOrDefault(d => d.Id_Docente == docente.Id_Docente);
+            if (existingDocente != null)
+            {
+                existingDocente.PrimerNombreD = docente.PrimerNombreD;
+                existingDocente.SegundoNombreD = docente.SegundoNombreD;
+                existingDocente.PrimerApellidoD = docente.PrimerApellidoD;
+                existingDocente.SegundoApellidoD = docente.SegundoApellidoD;
+                existingDocente.Email = docente.Email;
+                existingDocente.Id_titulo = docente.Id_titulo;
+            }
+        }
+
+        // Método para eliminar un docente
+        public void EliminarDocente(int id)
+        {
+            var docente = docentes.FirstOrDefault(d => d.Id_Docente == id);
+            if (docente != null)
+            {
+                docentes.Remove(docente);
+            }
+        }
+    }
+}
+>>>>>>> a04a6251e16735818b5cd903783d8e8b0b0da0ae
--- Controllers/TituloProfesionalController.cs
+++ Controllers/TituloProfesionalController.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -50,3 +51,57 @@ namespace _26_08_2024.Controladores
         }*/
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_08_2024.Controladores
+{
+    public class TituloProfesionalController
+    {/*
+        private List<TituloProfesional> titulosProfesionales = new List<TituloProfesional>();
+
+        // Método para agregar un título profesional
+        public void AgregarTituloProfesional(TituloProfesional titulo)
+        {
+            titulosProfesionales.Add(titulo);
+        }
+
+        // Método para obtener todos los títulos profesionales
+        public List<TituloProfesional> ObtenerTitulosProfesionales()
+        {
+            return titulosProfesionales;
+        }
+
+        // Método para actualizar un título profesional
+        public void ActualizarTituloProfesional(TituloProfesional titulo)
+        {
+            var tituloExistente = titulosProfesionales.FirstOrDefault(t => t.IdTitulo == titulo.IdTitulo);
+            if (tituloExistente != null)
+            {
+                tituloExistente.Nombre = titulo.Nombre;
+                tituloExistente.Descripcion = titulo.Descripcion;
+                tituloExistente.Institucion = titulo.Institucion;
+                tituloExistente.FechaObtencion = titulo.FechaObtencion;
+                tituloExistente.NivelAcademico = titulo.NivelAcademico;
+                tituloExistente.Especializacion = titulo.Especializacion;
+                tituloExistente.RegistroProfesional = titulo.RegistroProfesional;
+                tituloExistente.FechaRegistro = titulo.FechaRegistro;
+            }
+        }
+
+        // Método para eliminar un título profesional
+        public void EliminarTituloProfesional(int id)
+        {
+            var titulo = titulosProfesionales.FirstOrDefault(t => t.IdTitulo == id);
+            if (titulo != null)
+            {
+                titulosProfesionales.Remove(titulo);
+            }
+        }*/
+    }
+}
+>>>>>>> a04a6251e16735818b5cd903783d8e8b0b0da0ae
